bde4f738298aacc5d3217870e5c498ddc6e0da8a
Pedram <pedramshirin@hotmail.com>
2017-01-19 15:39:47 +0100

    Begin of Exerc_1_1
    Started off on Exerc_1_1 WP1 is just for training purposes

---
+++ Work_Package_1/Exerc_1_1/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/ * ====================================
+
+File name: exerc_1_1.c (or cpp)
+
+Date: 2017-mm-dd
+
+Group Number: 2
+
+Members that contributed:
+
+Pedram Shirinbolaghi
+
+Jonas Torstensson
+
+Robinson Iheanacho Mgbah
+
+Demonstration code: [ ]
+
+Important , No code no bonus !
+
+====================================== * /
+
+/*Write a program that reads in an integer number between 1 and 10 as input and prints out one of existing nine sentence in the console depending on what number was entered.
+The program exits on number 0.*/
+
+int main()
+{
+
+}

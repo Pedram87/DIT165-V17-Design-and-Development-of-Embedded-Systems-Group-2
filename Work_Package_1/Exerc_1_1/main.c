205019a6ec9c6d0833f7e0ec22b607029253818c
Pedram <pedramshirin@hotmail.com>
2017-01-19 16:10:43 +0100

    Exec_1_1 done
    Reads in an integer and prints the one before, really simple program.

--- Work_Package_1/Exerc_1_1/main.c
+++ Work_Package_1/Exerc_1_1/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/ * ====================================
+/* ====================================
 
 File name: exerc_1_1.c (or cpp)
 
@@ -21,12 +21,22 @@ Demonstration code: [ ]
 
 Important , No code no bonus !
 
-====================================== * /
+====================================== *\
 
 /*Write a program that reads in an integer number between 1 and 10 as input and prints out one of existing nine sentence in the console depending on what number was entered.
 The program exits on number 0.*/
 
 int main()
 {
+    // Integer to be entered
+    int input;
+  printf("Enter an integer 1-10:");
 
+
+ //Read in the entered integer
+ scanf("%d",&input);
+
+ //Print the result
+ if(input <= 10 && input >=0){printf("%d",input-1);}
+ return 0;
 }

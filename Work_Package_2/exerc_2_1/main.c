57bc6059cecb016d2c0836b9190cf849ddbf0d1c
Pedram <pedramshirin@hotmail.com>
2017-01-21 15:43:45 +0100

    Program instruction
    Added pdf instructions to exerc_2_1 as comments.

--- Work_Package_2/exerc_2_1/main.c
+++ Work_Package_2/exerc_2_1/main.c
@@ -23,6 +23,14 @@ Important , No code no bonus !
 
 ====================================== */
 
+/* Write a program that reads a string with a maximum of 20 characters from the keyboard and store it in a local string variable.
+Copy the string to another string by using:
+a) The library function strcpy(..)
+b) A new function void copyString(…) not using any library function.
+Main program ends by printing out the copied string in the console.
+The program should be able to read in from the keyboard or from a textfile ‘myfile.txt’ containing one string of characters. You create this file with notepad or any other editor. The reading from a text file should be done by redirect the reading from command line when program execution starts as follows:
+Exerc_2_1 < myfile.txt
+Where Exerc_2_1 is the filename of the compiled program.*/
 
 int main()
 {

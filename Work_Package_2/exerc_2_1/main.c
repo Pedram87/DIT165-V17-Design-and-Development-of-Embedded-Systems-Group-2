b51bd400b7be286dc7ef7ba1f471b242875b67bf
Pedram <pedramshirin@hotmail.com>
2017-01-21 15:35:09 +0100

    Initial commit of exerc_2_1
    First commit of exerc_2_1

---
+++ Work_Package_2/exerc_2_1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ====================================
+
+File name: exerc_x_y.c (or cpp)
+
+Date: 2017-mm-dd
+
+Group Number: 2
+
+Members that contributed:
+
+Pedram Shirinbolaghi
+
+Jonas Torstensson
+
+Robinson Iheanacho Mgbah
+
+Demonstration code: [ ]
+
+Important , No code no bonus !
+
+====================================== */
+
+
+int main()
+{
+
+    return 0;
+}

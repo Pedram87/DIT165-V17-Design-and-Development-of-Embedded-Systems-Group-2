24347d81b61ad7a93237e6e8c886525a6d9e8fa6
Pedram <pedramshirin@hotmail.com>
2017-01-24 22:50:13 +0100

    Exerc_2_5
    I think i have completed about half of the excercize, will continue
    tommorow too tired and hungry now :)

--- Work_Package_2/Exerc_2_5/main.c
+++ Work_Package_2/Exerc_2_5/main.c
@@ -24,8 +24,11 @@ Important , No code no bonus !
 
 ====================================== */
 
-/* You should develop a program that calculates some statistical values for an array of integers. Among other things, the program will plot a histogram for the frequency of the different input numbers in the array.
-To test it you need first to create an array of integers (table [MAX]) with MAX number of random numbers between 0 and MAXNUMBER. Then you should write a function that for each possible number between 0 – MAXNUMBER calculates how many times the number exists in the array. The result is then stored in a new array (frequency []).
+/* You should develop a program that calculates some statistical values for an array of integers. Among other things,
+ the program will plot a histogram for the frequency of the different input numbers in the array.
+To test it you need first to create an array of integers (table [MAX]) with MAX number of random numbers between 0 and MAXNUMBER.
+Then you should write a function that for each possible number between 0 – MAXNUMBER
+calculates how many times the number exists in the array. The result is then stored in a new array (frequency []).
 Finally, you write a function that given the array frequency [] draw a histogram as followed example:
 You should use the function and its function declaration below.
 Given an array table[]={ 1,2,12, 5,1,0,0,5,9,12, 0,2,3,0} thhe program will printout:
@@ -52,8 +55,28 @@ int frequency[MAXNUMBER];
 ….
 } */
 
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 100
+#define MAXNUMBER 20
 
-    return 0;
+//Declare functions as described.
+void create_random( int tab[] );
+void count_frequency(int tab[], int freq[] );
+void draw_histogram(int freq[] );
+
+//Declare main and array for table and frequency as described.
+int main ( void){
+int table[MAX], n ;
+int frequency[MAXNUMBER];
+
+//A function that for each possible number between 0 – MAXNUMBER
+//calculates how many times the number exists in the array.
+create_random(table[MAX]);
+//Loop through the array for table[MAX] to check if numbers between 0-MAXNUMBER exists.
+for(int i =0; i<MAXNUMBER; ++i)
+//Store the result variable i in frequency[] array.
+frequency[i] =0;
+
 }

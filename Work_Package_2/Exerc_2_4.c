9ae763421f45e38eb64d8235e2a38df64acad381
Pedram <pedramshirin@hotmail.com>
2017-01-30 22:19:49 +0100

    Update Exerc_2_4.c
    Updated header.

--- Work_Package_2/Exerc_2_4.c
+++ Work_Package_2/Exerc_2_4.c
@@ -1,6 +1,24 @@
-/*
-  author: mgbah robinson iheanacho
- */
+/* ====================================
+
+File name: exerc_2_4.c (or cpp)
+
+Date: 2017-mm-dd
+
+Group Number: 2
+
+Members that contributed:
+
+Pedram Shirinbolaghi
+
+Jonas Torstensson
+
+Robinson Iheanacho Mgbah
+
+Demonstration code: [ ]
+
+Important , No code no bonus !
+
+====================================== */
 
 #include <stdio.h>
 #include <stdlib.h>

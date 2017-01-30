95efe7a95a18e59f9965ce8190a0b895af040ae6
Pedram <pedramshirin@hotmail.com>
2017-01-30 22:18:07 +0100

    Update Exerc_2_2.c
    Added correct header, guys always remember to add the following header:
    /* ====================================
    
    File name: exerc_x_y.c (or cpp)
    
    Date: 2017-mm-dd
    
    Group Number: 2
    
    Members that contributed:
    
    Pedram Shirinbolaghi
    
    Jonas Torstensson
    
    Robinson Iheanacho Mgbah
    
    Demonstration code: [ ]
    
    Important , No code no bonus !
    
    ====================================== */

--- Work_Package_2/Exerc_2_2.c
+++ Work_Package_2/Exerc_2_2.c
@@ -1,7 +1,24 @@
-/*
-  mgbah robinson iheanacho
-  26th januari 2016
- */
+/* ====================================
+
+File name: exerc_2_2.c (or cpp)
+
+Date: 2017-mm-dd
+
+Group Number: 2
+
+Members that contributed:
+
+Pedram Shirinbolaghi
+
+Jonas Torstensson
+
+Robinson Iheanacho Mgbah
+
+Demonstration code: [ ]
+
+Important , No code no bonus !
+
+====================================== */
 
 #include <stdio.h>
 #include <stdlib.h>
